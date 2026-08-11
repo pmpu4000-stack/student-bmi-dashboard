3438a2b5eca68cc1ddf76179a4e2bf3116796695
Eric Lin <pmpu4000@gmail.com>
2026-08-11 12:04:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -422,4 +422,4 @@ def update_table(table_year, table_category):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
